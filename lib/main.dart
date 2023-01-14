c6aa630c8f3d1a7b70be1a68bf720883bf166346
veixhen <vxwilson@hotmail.com>
2023-01-14 11:35:29 +0800

    test change color

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Hive Figh RHB',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: const LoginPage(),
     );
