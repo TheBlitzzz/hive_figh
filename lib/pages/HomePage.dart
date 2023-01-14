7f73f40cad7a4273d528f8a297d6e2e42985aa2b
veixhen <vxwilson@hotmail.com>
2023-01-14 16:34:30 +0800

    -slightly more functional homescreen, added balance

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
             alignment: Alignment.topCenter,
             child: Column(
                 children: [
-                  SizedBox(height: 40.0),
+                  SizedBox(height: 45.0),
                   Container(
                       decoration: BoxDecoration(
                           color: Colors.cyan.withOpacity(0.5),
@@ -35,10 +35,134 @@ class _HomePageState extends State<HomePage> {
                     width: 300.0,
                     height: 150.0,
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Text("Remaining Balance: MYR 294.19")
+                        Image(image: AssetImage('assets/images/graph.png'),
+                          height: 120,
+                          width: 120,),
+                        Text("Remaining Balance: MYR 294.19"),
+                        SizedBox(height:5)
                       ],
                     )
+                  ),
+                  SizedBox(height:35),
+                  Text("Transactions"),
+                  Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+
+                          border: Border.all(
+                              color: Colors.grey.withOpacity(0.0)
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                      margin: const EdgeInsets.all(10.0),
+                      width: 300.0,
+                      height: 150.0,
+                      child: SingleChildScrollView(
+                          child:Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("John Smith "),
+                              Text("-MYR 200")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Emma Johnson "),
+                              Text("-MYR 150")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Michael Brown "),
+                              Text("-MYR 100")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Sarah Davis "),
+                              Text("-MYR 50")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("David Miller "),
+                              Text("-MYR 25")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("James Garcia "),
+                              Text("-MYR 20")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Emily Rodriguez "),
+                              Text("-MYR 15")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Matthew Wilson "),
+                              Text("-MYR 10")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Elizabeth Martinez "),
+                              Text("-MYR 5")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Joseph Anderson "),
+                              Text("-MYR 2")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Nicole Taylor "),
+                              Text("-MYR 1")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Anthony Perez "),
+                              Text("-MYR 0.5")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Mary White "),
+                              Text("-MYR 0.2")
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Kevin Lee "),
+                              Text("-MYR 0.1")
+                            ],
+                          ),
+                            ],
+                      ))
                   ),
                 ]
             )
